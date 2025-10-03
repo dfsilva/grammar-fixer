c6042956bc7534ab61f35b73f25664a064e8a283
Diego Silva <diegosiuniube@gmail.com>
2025-10-03 16:53:29 -0300

    Initial Commit

---
+++ grammar-fixer/grammar_fixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  grammar_fixerApp.swift
+//  grammar-fixer
+//
+//  Created by Diego Silva on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct grammar_fixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
